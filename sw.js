b1c8387d72c21fffb6dbb89bbe7b04ae5f7161b5
anirbanchowdhury <ani_anirban@yahoo.com>
2026-08-01 10:46:16 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,41 +1,20 @@
-const CACHE = 'bhoopali-practice-v1';
-const ASSETS = [
-  './',
-  './index.html',
-  './segments.json',
-  './bhoopali.mp3',
-  './app.webmanifest',
-  './icon-192.png',
-  './icon-512.png'
-];
+const CACHE = 'bhoopali-builder-v2';
+const ASSETS = ['./', './index.html', './app.webmanifest', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE).then((cache) => cache.addAll(ASSETS))
-  );
+  event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(ASSETS)));
   self.skipWaiting();
 });
 
 self.addEventListener('activate', (event) => {
   event.waitUntil(
-    caches.keys().then((keys) =>
-      Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)))
-    )
+    caches.keys().then((keys) => Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k))))
   );
   self.clients.claim();
 });
 
 self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.match(event.request).then((cached) => {
-      if (cached) return cached;
-      return fetch(event.request).then((response) => {
-        if (response.ok && event.request.method === 'GET') {
-          const clone = response.clone();
-          caches.open(CACHE).then((cache) => cache.put(event.request, clone));
-        }
-        return response;
-      }).catch(() => cached);
-    })
+    caches.match(event.request).then((cached) => cached || fetch(event.request))
   );
 });
